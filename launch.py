942d08127e3c6012c4a4fac1beb7db48bf7eb4b2
Fabrizio <f4brizio.21@gmail.com>
2023-06-24 11:31:09 -0500

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,5 +1,6 @@
 from modules import launch_utils
 
+
 args = launch_utils.args
 python = launch_utils.python
 git = launch_utils.git
@@ -22,50 +23,8 @@ prepare_environment = launch_utils.prepare_environment
 configure_for_tests = launch_utils.configure_for_tests
 start = launch_utils.start
 
-import threading
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-
-class NewFileHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        if event.is_directory:
-            return
-        print(f"Se ha creado un nuevo archivo: {event.src_path}")
-
-
-def start_observer(path):
-    event_handler = NewFileHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=False)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
-
 
 def main():
-    print("Iniciando... main()")
-    path = "/kaggle/working/stable-diffusion-webui/tmp/"  # Ruta que deseas monitorear
-
-    observer_thread = threading.Thread(target=start_observer, args=(path,))
-    observer_thread.start()
-    print("observer_thread... start()")
-
-    path2 = "/kaggle/working/stable-diffusion-webui/outputs/"  # Ruta que deseas monitorear
-
-    observer_thread2 = threading.Thread(target=start_observer, args=(path2,))
-    observer_thread2.start()
-    print("observer_thread2... start()")
-
-    print("Iniciando... v0.2 f4brizio")
-
     if not args.skip_prepare_environment:
         prepare_environment()
 
